002bf60b5acd27b3d6838d5545b846da663b0173
ialloyd <iamalloydfds@gmail.com>
2023-10-31 17:54:14 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
 import React from "react";
 import './../styles/App.css';
+import { increment, decrement } from "../redux/actions/counterAction";
+import { useSelector, useDispatch } from "react-redux";
 
 const App = () => {
+
+  const counter = useSelector(state => state)
+
+  const dispatch = useDispatch()
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Counter</h1>
+      <p>{counter}</p>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   )
 }
---
+++ src/redux/reducer/counterReducer.js
@@ -0,0 +1,15 @@
+import { INCREMENT, DECREMENT } from "../actions/actionTypes";
+
+const counterReducer = (count = 0, action) => {
+
+    switch (action.type) {
+
+        case INCREMENT: return count + 1;
+        case DECREMENT: return count - 1;
+        default: return count;
+    }
+
+
+}
+
+export default counterReducer
